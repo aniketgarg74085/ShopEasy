1816011f637ce9517045ee98f967d71afd9b6e4b
Atish Salgotra <salgotra.atish9@gmail.com>
2021-12-10 17:08:37 +0530

    End of Part 2 of the Project

--- src/Product.js
+++ src/Product.js
@@ -1,35 +1,21 @@
-import React from 'react'
-import "./css/product.css"
+import React from "react";
+import "./css/product.css";
 
-function Product() {
-    return (
-        <div className="products_wrap">
-            <div className="product">
-                <img src ="/images/phone.jpeg"/>
-                <h3>Product Name</h3>
-                <p>Product Price</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <img src ="/images/phone.jpeg"/>
-                <h3>Product Name</h3>
-                <p>Product Price</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <img src ="/images/phone.jpeg"/>
-                <h3>Product Name</h3>
-                <p>Product Price</p>
-                <button>Add to Cart</button>
-            </div>
-            <div className="product">
-                <img src ="/images/phone.jpeg"/>
-                <h3>Product Name</h3>
-                <p>Product Price</p>
-                <button>Add to Cart</button>
-            </div>
-        </div>
-    )
+function Product({ productsList, addToCart}) {
+  return (
+    <div className="products_wrap">
+      {productsList && productsList.map((items) => {
+        return (
+          <div className="product" key={items.id}>
+            <img src={items.image.url} alt="product"/>
+            <h3>{items.name}</h3>
+            <p>{items.price.formatted_with_symbol}</p>
+            <button onClick={()=>addToCart(items.id, 1)}>Add to Cart</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Product
+export default Product;
---
+++ src/lib/commerce.js
@@ -0,0 +1,5 @@
+import Commerce from "@chec/commerce.js"
+
+const commerce = new Commerce('pk_test_369796319041fc81beef59d12cae6beb0dd9f170eee1c');
+
+export default commerce
